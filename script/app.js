e194ac5a93de5aa3c293b0785294bc334e9ff810
Oluseun <oluseun.akintayo@gmail.com>
2020-11-20 01:02:27 +0100

    more dashboard changes

---
+++ script/app.js
@@ -0,0 +1,35 @@
+// validate the password on the register page
+/* function validation() {
+    let password1 = document.getElementById('pwd-i');
+    let password2 = document.getElementById('pwd-ii');
+
+    if(password1.value.trim() !== password2.value.trim()) {
+        alert('The entered passwords do not match');
+        return false;
+    } else {
+        return true;
+    }
+}
+*/
+
+function menuFunction() {
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
+
+// Sticky navbar //
+window.onscroll = function() { stickyHeader() };
+let topContent = document.getElementById('header-content');
+let sticky = topContent.offsetTop;
+
+function stickyHeader() {
+    if (window.pageYOffset >= sticky) {
+        topContent.classList.add("sticky")
+    } else {
+        topContent.classList.remove("sticky");
+    }
+}
